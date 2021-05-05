82b6b3b2aa9a09d6cf735e1005137ae00a86b497
Rydal <i.am.alex2k@gmail.com>
2021-05-06 01:55:05 +0300

    added SPA for deploy

--- routes/web.php
+++ routes/web.php
@@ -13,9 +13,13 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/admin', function () {
+    return view('admin-panel');
+})->where('any', '.+');
+
+Route::get('/{any?}', function () {
+    return view('index');
+})->where('any', '.+');
 
 Route::middleware(['auth:sanctum', 'verified'])->get('/dashboard', function () {
     return view('dashboard');
