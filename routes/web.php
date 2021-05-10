d5a9df565da5372108c7ca1019d8d89459808e07
Rydal <i.am.alex2k@gmail.com>
2021-05-10 20:32:01 +0300

    changed routes

--- routes/web.php
+++ routes/web.php
@@ -13,7 +13,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/admin', function () {
+Route::get('/admin/{any?}', function () {
     return view('admin-panel');
 })->where('any', '.+');
 
